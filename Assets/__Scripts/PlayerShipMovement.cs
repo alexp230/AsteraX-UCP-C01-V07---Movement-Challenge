973ee949f88c9562e362c3672d2833b461cc016f
alexp230 <apruitt230@gmail.com>
2025-02-03 18:50:07 -0600

    Added movement and rotation for the player ship

---
+++ Assets/__Scripts/PlayerShipMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerShipMovement : MonoBehaviour
+{
+    private float Speed = 400f;
+    private Rigidbody _RigidBody;
+
+
+    void Start()
+    {
+        _RigidBody = this.GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") ,Input.GetAxis("Vertical"), 0).normalized;
+        moveCharacter(movement);
+
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = transform.position.z; // Keep the z position the same
+
+        Vector3 direction = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+
+    void moveCharacter(Vector3 direction)
+    {
+        _RigidBody.velocity = direction * Speed * Time.fixedDeltaTime;
+    }
+}
