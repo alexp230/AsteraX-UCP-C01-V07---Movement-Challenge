bf075c5738bbb82b05b0d8a714ab3dbb071405ea
alexp230 <apruitt230@gmail.com>
2025-02-03 19:18:26 -0600

    Bullet now instantiates under BulletAnchor Transform

--- Assets/__Scripts/PlayerShipFiring.cs
+++ Assets/__Scripts/PlayerShipFiring.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class PlayerShipFiring : MonoBehaviour
 {
     [SerializeField] private GameObject Bullet_Prefab;
+    [SerializeField] private Transform BulletParent;
     private PlayerShipMovement PlayerShipMovement_S;
+    
 
     private const float BULLET_SPEED = 500f;
 
@@ -17,7 +19,7 @@ public class PlayerShipFiring : MonoBehaviour
         // if (Input.GetAxis("Fire1") != 0)
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            GameObject bullet = Instantiate(Bullet_Prefab, this.transform.position, Quaternion.identity);
+            GameObject bullet = Instantiate(Bullet_Prefab, this.transform.position, Quaternion.identity, BulletParent);
             bullet.GetComponent<Rigidbody>().AddForce(PlayerShipMovement_S.MousePosition * BULLET_SPEED);            
         }
     }
